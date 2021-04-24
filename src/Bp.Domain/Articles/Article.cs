5856a20d4bd513e864cc1f732c4b7d8363ff2979
Vladi <vladislav.budnikov@gmail.com>
2021-04-24 20:46:19 +0300

    Adding Article entity

---
+++ src/Bp.Domain/Articles/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Bp.Articles
+{
+    public class Article : AuditedAggregateRoot<Guid>
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+
+    }
+}
