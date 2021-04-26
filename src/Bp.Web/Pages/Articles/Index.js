e5b0d1c62225bceb9f30f75b9b99e43fa6cd15ed
Vladi <vladislav.budnikov@gmail.com>
2021-04-26 21:26:50 +0300

    Adding Export to Excel functionality

--- src/Bp.Web/Pages/Articles/Index.js
+++ src/Bp.Web/Pages/Articles/Index.js
@@ -1,16 +1,19 @@
 ﻿$(function () {
     var l = abp.localization.getResource('Bp');
-    var dataTable = $('#ArticlesTable').DataTable({
-        buttons: [
-            'excel'
-        ]
-    },
+    var dataTable = $('#ArticlesTable').DataTable(
         abp.libs.datatables.normalizeConfiguration({
             serverSide: true,
             paging: true,
             order: [[1, "asc"]],
             searching: false,
             scrollX: true,
+            dom: 'Bfrtip',
+            buttons: [
+                {
+                    extend: 'excel',
+                    text: 'Export to excel'
+                },
+            ],
             ajax: abp.libs.datatables.createAjax(bp.articles.article.getList),
             columnDefs: [
                 {
@@ -21,7 +24,7 @@
                 {
                     title: l('Author'),
                     data: "author"
-                }                
+                }
             ]
         })
     )
