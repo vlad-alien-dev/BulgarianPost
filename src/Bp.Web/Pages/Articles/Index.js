8379fd4927b618d86df68d0f17b06fc3be99276b
Vladi <vladislav.budnikov@gmail.com>
2021-04-26 22:08:40 +0300

    Adding more bg localization, removing unneeded languages, giving the application a brand name

--- src/Bp.Web/Pages/Articles/Index.js
+++ src/Bp.Web/Pages/Articles/Index.js
@@ -2,7 +2,7 @@
     var l = abp.localization.getResource('Bp');
     var dataTable = $('#ArticlesTable').DataTable(
         abp.libs.datatables.normalizeConfiguration({
-            serverSide: true,
+            serverSide: false,
             paging: true,
             order: [[1, "asc"]],
             searching: false,
@@ -11,7 +11,7 @@
             buttons: [
                 {
                     extend: 'excel',
-                    text: 'Export to excel'
+                    text: l('Export to excel')
                 },
             ],
             ajax: abp.libs.datatables.createAjax(bp.articles.article.getList),
---
+++ src/Bp.Web/ProjectBrandingProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace Bp.Web
+{
+    [Dependency(ReplaceServices = true)]
+    public class ProjectBrandingProvider: DefaultBrandingProvider
+    {
+        public override string AppName => "Bulgarian Post";
+
+    }
+}
